b93c531ec293aa52e900574ad5de142b78b46b5c
LandonDev <landonhale123@gmail.com>
2021-11-26 00:45:34 -0600

    Overrode /buy to be useable

--- src/main/java/landon/ingamepurchases/playerdata/PlayerDataListener.java
+++ src/main/java/landon/ingamepurchases/playerdata/PlayerDataListener.java
@@ -1,12 +1,23 @@
 package landon.ingamepurchases.playerdata;
 
+import com.mysql.jdbc.StringUtils;
+import landon.ingamepurchases.gui.ShopInventory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 public class PlayerDataListener implements Listener {
     @EventHandler
     public void updatePlayerUsernameEvent(PlayerLoginEvent e) {
         PlayerDataManager.get().updateUsername(e.getPlayer());
+    }
+
+    @EventHandler
+    public void buyCommand(PlayerCommandPreprocessEvent e) {
+        if(StringUtils.startsWithIgnoreCase(e.getMessage(), "/buy")) {
+            e.setCancelled(true);
+            ShopInventory.build().open(e.getPlayer());
+        }
     }
 }
